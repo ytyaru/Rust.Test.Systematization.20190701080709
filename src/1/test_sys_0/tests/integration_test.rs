da56d57e7014e4210c1474041f57e23c084f0eeb
ytyaru <ytyaru@outlook.jp>
2019-07-01 08:53:21 +0900

    初回コミット

---
+++ src/1/test_sys_0/tests/integration_test.rs
@@ -0,0 +1,12 @@
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, test_sys_0::add_two(2));
+}
+/*
+extern crate test_sys_0;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, test_sys_0::add_two(2));
+}
+*/
---
+++ src/2/test_sys_0/tests/integration_test.rs
@@ -0,0 +1,8 @@
+mod common;
+
+#[test]
+fn it_adds_two() {
+    common::setup();
+    assert_eq!(4, test_sys_0::add_two(2));
+}
+
